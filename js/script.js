0fc6914f647239afc2a8f4ef4207f570cb7f648c
daraujo92 <46228002+daraujo92@users.noreply.github.com>
2022-01-23 19:05:41 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// function that reveals players list
+function revealMessage() {
+    document.getElementById("bottomHalf").style.display = "block";
+
+}
+
+
+
+// function that adds a player to the list --- START
+// https://stackoverflow.com/questions/19908215/adding-user-input-to-a-list-of-text-items-on-a-html-page
+// Moved to another filr (newPlayer.js)
+document.getElementById("submitButton").onclick = function () {
+
+    var node = document.createElement("Li");
+    var text = document.getElementById("formText").value;
+    var textnode = document.createTextNode(text);
+    node.appendChild(textnode);
+    document.getElementById("players").appendChild(node);
+}
+// function that adds a player to the list --- END
+
+function callTwo() { revealMessage(); newPlayer(); }

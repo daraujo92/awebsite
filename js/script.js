53db14b1cdd25d9a5cbb5bd275dd85d5ee2fb54a
daraujo92 <46228002+daraujo92@users.noreply.github.com>
2022-01-23 19:50:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,21 +2,26 @@
 function revealMessage() {
     document.getElementById("bottomHalf").style.display = "block";
 
-}
+    document.getElementById("submitButton").onclick = function () {
 
+        var node = document.createElement("Li");
+        var text = document.getElementById("formText").value;
+        var textnode = document.createTextNode(text);
+        node.appendChild(textnode);
+        document.getElementById("players").appendChild(node);
+    }
+}
 
 
 // function that adds a player to the list --- START
 // https://stackoverflow.com/questions/19908215/adding-user-input-to-a-list-of-text-items-on-a-html-page
 // Moved to another filr (newPlayer.js)
-document.getElementById("submitButton").onclick = function () {
+//document.getElementById("submitButton").onclick = function () {
 
-    var node = document.createElement("Li");
-    var text = document.getElementById("formText").value;
-    var textnode = document.createTextNode(text);
-    node.appendChild(textnode);
-    document.getElementById("players").appendChild(node);
-}
+//    var node = document.createElement("Li");
+//    var text = document.getElementById("formText").value;
+//   var textnode = document.createTextNode(text);
+//    node.appendChild(textnode);
+//    document.getElementById("players").appendChild(node);
+//}
 // function that adds a player to the list --- END
-
-function callTwo() { revealMessage(); newPlayer(); }
